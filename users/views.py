28c92eee9e636759e5d24f2b50b3c588e54eabab
cairomedeiros <cairomedeiros13@gmail.com>
2024-06-19 21:19:20 -0300

    users view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import logging
+from .serializers import UserSerializer
+from api.mongodb import get_db_handle
 
-# Create your views here.
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s')
+logging.getLogger().setLevel(logging.INFO)
+
+class UserView(APIView):
+    def get(self, request, format=None):
+        try:
+            collection = get_db_handle().users
+            users = list(collection.find({}))
+            serializer = UserSerializer(users, many=True)
+
+            logging.info(f"Successfully retrieved {len(users)} users")
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            logging.error(f"An error occurred: {str(e)}", exc_info=True)
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def post(self, request, format=None):
+        try:
+            serializer = UserSerializer(data=request.data)
+            if serializer.is_valid():
+                collection = get_db_handle().users
+                result = collection.insert_one(serializer.validated_data)
+                new_user = collection.find_one({"_id": result.inserted_id})
+
+                new_user_serializer = UserSerializer(new_user)
+
+                logging.info(f"Successfully created user with id {result.inserted_id}")
+
+                return Response(new_user_serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                logging.error(f"Serializer inválido: {serializer.errors}")
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            logging.error(f"An error occurred: {str(e)}", exc_info=True)
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
